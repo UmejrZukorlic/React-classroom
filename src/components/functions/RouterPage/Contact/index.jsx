0e0c21d24d1b14df3287188b4981e508c05bf46d
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2022-08-18 15:26:13 +0200

    Add files via upload

--- src/components/functions/RouterPage/Contact/index.jsx
+++ src/components/functions/RouterPage/Contact/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import Layout from '../Layout'
+import style from './style.css'
 
 function Cointact() {
   return (
     <>
     <Layout>
-        <div>Contact</div>
+        <div className='contactSection'>
+            <div className='contactHeader'>
+                <h1>Contact Us</h1>
+                <p>Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            </div>
+            <div className='Inputs'>
+                <div className='nameAndEmail'>
+                    <div>
+                        <label htmlFor="">Name</label>
+                        <input type="text" />
+
+                    </div>
+                    <div>
+                        <label htmlFor="">Email</label>
+                        <input type="text" />
+
+                    </div>
+
+                </div>
+                <div className='message'>
+                    <label htmlFor="">Message</label>
+                    <input type="text" />
+                </div>
+                <button>Button</button>
+            </div>
+        </div>
     </Layout>
     </>
   )
